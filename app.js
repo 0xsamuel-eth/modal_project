5a9d9f695a7d829058d762a3ef197ef1f49db90b
0xsamuel-eth <sammmastenn@gmail.com>
2023-07-30 12:37:23 -0400

    Added eventListener to remove open-modal from modalOverlay when a user clicks close-btn

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click", function() {
-    modal-modalOverlay.classList.add(".open-modal")
-})
+    modalOverlay.classList.add("open-modal")
+});
 
+closeBtn.addEventListener("click", function() {
+    modalOverlay.classList.remove("open-modal")
+});
